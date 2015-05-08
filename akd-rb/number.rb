168f1d8222e92bd7af04202e2caa7dfb8b45240e
Alexey Kudryashov <kudryashov.alexey.v@gmail.com>
2015-05-08 10:01:38 +0300

    Create dictionary.

--- akd-rb/number.rb
+++ akd-rb/number.rb
@@ -1,24 +1,22 @@
-file = File.expand_path(File.join(File.dirname(__FILE__), '..', 'dictionary.txt'))
-words = File.open(file).select { |w| w =~ /^\w+$/ }.map(&:chomp)
-d = words.group_by{|i| i[0].downcase}
+# FILE = File.expand_path(File.join(File.dirname(__FILE__), '..', 'dictionary.txt'))
+# WORDS = File.open(FILE).select { |w| w =~ /^\w+$/ }.map(&:chomp)
+FILE = File.expand_path(File.join(File.dirname(__FILE__), '..', 'd.txt'))
+WORDS = File.open(FILE).select { |w| w =~ /^\w+$/ }.map(&:chomp)
+DICTIONARY = dictionary(WORDS)
 
-
-dictionary = words.group_by{|i| i[index].downcase}
-letters =('a'..'z').to_a
-for i in letters
-  res[i] = res[i].group_by{|i| i[index+1].downcase} unless res[i].nil?
+def dictionary(array, index = 0)
+  res = array.group_by{|i| i[index].downcase}
+  dictionary = {}
+  index = index + 1
+  res.each do |key, value|
+    dictionary[key] = value.group_by{|i| i[index].downcase}
+  end
+  dictionary
+  end
 end
 
 module PhoneConverter
-  dic_file = File.expand_path(File.join(File.dirname(__FILE__), '..', 'dictionary.txt'))
-  WORDS = File.open(file).select { |w| w =~ /^\w+$/ }.map(&:chomp)
   CHARS_MAP = {"2" => "ABC", "3" => "DEF", "4" => "GHI", "5" => "JKL", "6" => "MNO", "7" => "PQRS", "8" => "TUV", "9" => "WXYZ" }
-
-  def dictionary(array, index = 0)
-    res = array.group_by{|i| i[index].downcase}
-    letters =('a'..'z').to_a
-    dictionary(res[letters[index]], (index + 1))
-  end
 
   def self.convert(phone)
     [].tap do |mem|
